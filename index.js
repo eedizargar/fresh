9593498adebaf78ba0a62a13f310cd4bfecb3e22
eedizargar <eedizargarxen>
2023-05-05 21:52:24 +0530

    added changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const routes = require("../fresh/routers");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(('/home'),routes);
+app.use('/',routes);
 const port = 3000;
 app.listen(port,()=>{
     console.log(`listening on port ${port}`);
--- routers.js
+++ routers.js
@@ -4,4 +4,10 @@ router.get("/firstPage",(req,res)=>{
     res.status(200).send();
 
 });
+router.get("/firstPage1",(req,res)=>{
+   console.log("hit 2 here");
+    //function will add the info to db
+    res.status(200).send("message");
+
+});
 module.exports = router;
